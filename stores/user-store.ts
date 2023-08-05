8745f95ab339b2a12e976411d788c9625a67b51c
Thomas Mello <work.mello@gmail.com>
2023-08-05 14:47:46 +0300

    remove useAuth, handle auth in store to prevent hydration mismatch

--- stores/user-store.ts
+++ stores/user-store.ts
@@ -1,18 +1,11 @@
 import { acceptHMRUpdate, defineStore, skipHydrate } from 'pinia';
 import type { User } from 'firebase/auth';
-import { useAuth } from '@vueuse/firebase/useAuth';
 import { useGoogleAuthStore } from '~/stores/google-auth-store';
 
 export const useUserStore = defineStore('user', () => {
   const { $auth } = useNuxtApp();
 
-  let user = ref<User | null>(null);
-  let isAuthenticated = computed(() => false);
-  if ($auth) {
-    const authObject = useAuth($auth);
-    user = authObject.user;
-    isAuthenticated = authObject.isAuthenticated;
-  }
+  const user = ref<User | null>(null);
   const idToken = useCookie('idToken', {
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
@@ -24,19 +17,20 @@ export const useUserStore = defineStore('user', () => {
   // true if there is a user token stored in the system
   // indicates that the user was logged in at some point (primarily for ssr)
   const isLoggedIn = computed(() => !!idToken.value);
+  // true if Firebase has confirmed that the user is logged in
+  const isAuthenticated = computed(() => !!user.value);
 
   // watch only on the client side
-  if ($auth) {
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    watchEffect(async () => {
-      if (user.value) {
-        idToken.value = await user.value.getIdToken();
-      } else {
-        idToken.value = null;
-        authorizationInfo.value = { accessToken: '', expiry: 0 };
-      }
-    });
-  }
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  $auth && $auth.onIdTokenChanged(async (authUser) => {
+    user.value = authUser;
+    if (user.value) {
+      idToken.value = await user.value.getIdToken();
+    } else {
+      idToken.value = null;
+      authorizationInfo.value = { accessToken: '', expiry: 0 };
+    }
+  });
 
   return {
     user,
