271291008c7b66aab599f39d66b6d5bae310afba
Thomas Mello <work.mello@gmail.com>
2023-08-14 16:28:23 +0300

    add better error handling and loading state

--- composables/useDriveFile.ts
+++ composables/useDriveFile.ts
@@ -1,22 +1,26 @@
-import type { MaybeRef, Ref } from 'vue';
+import type { MaybeRef } from 'vue';
 import type { DriveFile } from '~/models/types';
 import { extractErrorMessage } from '~/utils/extractErrorMessage';
 
 const fieldMask = 'id, trashed, name, originalFilename, mimeType, shared, isAppAuthorized, imageMediaMetadata, createdTime, modifiedTime, fileExtension, properties, appProperties, md5Checksum, version, videoMediaMetadata, thumbnailLink, permissionIds, size, quotaBytesUsed, capabilities' as const;
 
-export const useDriveFile = (id: MaybeRef<string | null>): Ref<DriveFile | null> => {
+export const useDriveFile = (id: MaybeRef<string | null>) => {
   const driveStore = useDriveStore();
   const { isReady } = toRefs(driveStore);
 
-  const file = ref<DriveFile | null>(null);
+  const file = shallowRef<DriveFile | null>(null);
+  const error = shallowRef<unknown>(null);
+  const isLoading = ref(false);
   const idRef = ref(id);
 
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   watchEffect(async () => {
-    console.log(`watching: id: ${idRef.value}, ready: ${isReady.value}`);
     if (!isReady.value || !idRef.value) {
       return;
     }
+
+    error.value = null;
+    isLoading.value = true;
 
     try {
       const client = await driveStore.getClient();
@@ -28,10 +32,14 @@ export const useDriveFile = (id: MaybeRef<string | null>): Ref<DriveFile | null>
       file.value = response.result as DriveFile;
     } catch (e) {
       file.value = null;
+      error.value = e;
+
       const notificationStore = useNotificationStore();
       notificationStore.error(extractErrorMessage(e));
+    } finally {
+      isLoading.value = false;
     }
   });
 
-  return file;
+  return { file, error, isLoading };
 };
