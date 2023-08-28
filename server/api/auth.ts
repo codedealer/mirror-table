b6e1bd250cebc11150578a16542d126be5d66032
Thomas Mello <work.mello@gmail.com>
2023-08-29 01:28:31 +0300

    remove api entry

--- server/api/auth.ts
+++ server/api/auth.ts
@@ -1,40 +1,5 @@
-import type { App, ServiceAccount } from 'firebase-admin/app';
-import { cert, getApps, initializeApp } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
 
-export default defineEventHandler(async (event) => {
-  const apps = getApps();
-  let app: App;
-  if (apps.length === 0) {
-    const serviceAccount = JSON.parse(
-      process.env.FB_SERVICE_ACCOUNT as string,
-    ) as ServiceAccount;
-    app = initializeApp({
-      credential: cert(serviceAccount),
-    });
-  } else {
-    app = apps[0];
-  }
 
-  const idToken = getCookie(event, 'idToken');
-
-  if (!idToken) {
-    throw createError({
-      statusCode: 401,
-      statusMessage: 'Unauthorized',
-    });
-  }
-
-  const auth = getAuth(app);
-
-  try {
-    await auth.verifyIdToken(idToken);
-  } catch (e) {
-    throw createError({
-      statusCode: 401,
-      statusMessage: 'Invalid Id token supplied',
-    });
-  }
-
+export default defineEventHandler((event) => {
   return { valid: true, authenticated: true };
 });
