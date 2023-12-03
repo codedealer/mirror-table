92d27f77f9fc4819da038fece10386021b00a639
Thomas Mello <work.mello@gmail.com>
2023-12-04 02:32:35 +0300

    add core plugins to markdown-it

--- components/WindowContainerMarkdownRenderer.vue
+++ components/WindowContainerMarkdownRenderer.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 import MarkdownIt from 'markdown-it';
+import MarkdownItAbbr from 'markdown-it-abbr';
+import MarkdownItFootnote from 'markdown-it-footnote';
+import MarkdownItSub from 'markdown-it-sub';
+import MarkdownItSup from 'markdown-it-sup';
+import MarkdownItAnchor from 'markdown-it-anchor';
+import MarkdownItTaskLists from 'markdown-it-task-lists';
 
 defineProps<{
   source: string
 }>();
 
-const md = new MarkdownIt();
+const md = new MarkdownIt()
+  .use(MarkdownItAbbr)
+  .use(MarkdownItFootnote)
+  .use(MarkdownItSub)
+  .use(MarkdownItSup)
+  .use(MarkdownItAnchor)
+  .use(MarkdownItTaskLists, { enabled: true });
 </script>
 
 <template>
