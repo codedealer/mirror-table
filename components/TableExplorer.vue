875945592b4fbf4e3ba073be579c3d1565cb41fb
Thomas Mello <work.mello@gmail.com>
2023-12-24 02:13:28 +0300

    add transition to table explorer

--- components/TableExplorer.vue
+++ components/TableExplorer.vue
@@ -3,24 +3,26 @@ const tableExplorerStore = useTableExplorerStore();
 </script>
 
 <template>
-  <div class="explorer">
-    <div v-if="tableExplorerStore.isReady" class="explorer-content">
-      <LazyTableExplorerTree />
+  <transition appear name="slide-fade">
+    <div class="explorer">
+      <div v-if="tableExplorerStore.isReady" class="explorer-content">
+        <LazyTableExplorerTree />
+      </div>
+      <div v-show="!tableExplorerStore.isReady" class="explorer-skeleton">
+        <VaSkeletonGroup animation="wave">
+          <div class="explorer-skeleton-header mb">
+            <va-skeleton variant="rounded" inline width="64px" height="32px" />
+            <va-skeleton variant="rounded" inline width="64px" height="32px" />
+          </div>
+          <div class="explorer-skeleton-content">
+            <va-skeleton variant="rounded" height="32px" />
+            <va-skeleton variant="rounded" height="32px" />
+            <va-skeleton variant="rounded" height="32px" />
+          </div>
+        </VaSkeletonGroup>
+      </div>
     </div>
-    <div v-show="!tableExplorerStore.isReady" class="explorer-skeleton">
-      <VaSkeletonGroup animation="wave">
-        <div class="explorer-skeleton-header mb">
-          <va-skeleton variant="rounded" inline width="64px" height="32px" />
-          <va-skeleton variant="rounded" inline width="64px" height="32px" />
-        </div>
-        <div class="explorer-skeleton-content">
-          <va-skeleton variant="rounded" height="32px" />
-          <va-skeleton variant="rounded" height="32px" />
-          <va-skeleton variant="rounded" height="32px" />
-        </div>
-      </VaSkeletonGroup>
-    </div>
-  </div>
+  </transition>
 </template>
 
 <style scoped lang="scss">
