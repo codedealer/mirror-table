f84fd2ed6b7cabc1ca6bd9f79e8a93e83d24c1a5
Thomas Mello <work.mello@gmail.com>
2025-03-04 20:46:04 +0300

    refactor: remove explicit file fetch, logic moved into saveFile method

--- utils/updateComplexAssetProperties.ts
+++ utils/updateComplexAssetProperties.ts
@@ -3,18 +3,6 @@ import type { AssetProperties } from '~/models/types';
 const updateComplexAssetProperties = async (fileId: string, properties: AssetProperties) => {
   const driveFileStore = useDriveFileStore();
 
-  // ensure the file is loaded
-  if (!driveFileStore.files[fileId]) {
-    try {
-      await driveFileStore.getFile(fileId);
-    } catch (e) {
-      const notificationStore = useNotificationStore();
-      notificationStore.error(extractErrorMessage(e));
-      console.error(e);
-    }
-  }
-
-  // assume the file is loaded
   try {
     await driveFileStore.saveFile(fileId, properties);
   } catch (e) {
