f38929034766c037f4c677db26d4fc29c5fcf464
Thomas Mello <work.mello@gmail.com>
2025-03-15 04:01:52 +0300

    fix: selection resets when updating a scene element

--- composables/useSelectableStateWatcher.ts
+++ composables/useSelectableStateWatcher.ts
@@ -10,34 +10,36 @@ export const useSelectableStateWatcher = (
   const { hideHiddenElements, activeGroups } = storeToRefs(layersStore);
   const { mode } = storeToRefs(tableStore);
 
-  const stopWatcher = watch(
-    [
-      () => element.value.selectionGroup,
-      hideHiddenElements,
-      mode,
-      activeGroups,
-    ],
-    ([selectionGroup, hide, mode, activeGroupsVal]) => {
-      if (!state.value) {
-        return;
-      }
-      if (mode !== TableModes.OWN) {
-        updateFn({
-          selectable: false,
-          selected: false,
-        });
-
-        return;
-      }
-
-      const isActiveGroup = activeGroupsVal[selectionGroup] === true;
+  const stopWatcher = watch([
+    () => element.value.selectionGroup,
+    hideHiddenElements,
+    mode,
+    activeGroups,
+  ], (
+    [selectionGroup, hide, mode, activeGroupsVal],
+  ) => {
+    if (!state.value) {
+      return;
+    }
+    if (mode !== TableModes.OWN) {
       updateFn({
-        selectable: hide ? (isActiveGroup && element.value.enabled) : isActiveGroup,
+        selectable: false,
         selected: false,
       });
-    },
-    { deep: true, immediate: true },
-  );
+
+      return;
+    }
+
+    const isActiveGroup = activeGroupsVal[selectionGroup] === true;
+    const selectable = hide ? (isActiveGroup && element.value.enabled) : isActiveGroup;
+    const payload: Partial<CanvasElementState> = {
+      selectable,
+    };
+    if (!selectable) {
+      payload.selected = false;
+    }
+    updateFn(payload);
+  }, { deep: true, immediate: true });
 
   onBeforeUnmount(stopWatcher);
 
